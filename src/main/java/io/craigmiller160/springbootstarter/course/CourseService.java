7ea4d9e1dc8d5002e09467a8d78a6f06a3d46493
Craig Miller <craigmiller160@gmail.com>
2018-11-03 11:02:06 -0400

    Added spring transaction support

--- src/main/java/io/craigmiller160/springbootstarter/course/CourseService.java
+++ src/main/java/io/craigmiller160/springbootstarter/course/CourseService.java
@@ -3,13 +3,12 @@ package io.craigmiller160.springbootstarter.course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CourseService {
-
-    //TODO add spring transaction handling to these methods
 
     private final CourseRepository courseRepository;
 
@@ -18,6 +17,7 @@ public class CourseService {
         this.courseRepository = courseRepository;
     }
 
+    @Transactional
     public Optional<List<Course>> getAllCourses(String topicId) {
         List<Course> courses = courseRepository.findByTopicId(topicId);
 
@@ -27,20 +27,24 @@ public class CourseService {
         return Optional.of(courses);
     }
 
+    @Transactional
     public Optional<Course> getCourse(String id) {
         return courseRepository.findById(id);
     }
 
+    @Transactional
     public Optional<Course> addCourse(Course course) {
         course = courseRepository.save(course); //TODO this creates and updates together... wtf...
         return Optional.of(course);
     }
 
+    @Transactional
     public Optional<Course> updateCourse(String id, Course course) {
         course = courseRepository.save(course);
         return Optional.of(course); //TODO this doesn't distinguish between something new or something updated
     }
 
+    @Transactional
     public Optional<Course> deleteCourse(String id) {
         Optional<Course> course = courseRepository.findById(id);
         courseRepository.deleteById(id); //TODO don't like how this doesn't return anything...
--- src/main/java/io/craigmiller160/springbootstarter/topic/TopicService.java
+++ src/main/java/io/craigmiller160/springbootstarter/topic/TopicService.java
@@ -3,6 +3,7 @@ package io.craigmiller160.springbootstarter.topic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -11,8 +12,6 @@ import java.util.stream.StreamSupport;
 @Service
 public class TopicService {
 
-    //TODO add spring transaction handling to these methods
-
     private final TopicRepository topicRepository;
 
     @Autowired
@@ -20,6 +19,7 @@ public class TopicService {
         this.topicRepository = topicRepository;
     }
 
+    @Transactional
     public Optional<List<Topic>> getAllTopics() {
         List<Topic> topics = StreamSupport.stream(topicRepository.findAll().spliterator(),  false)
                 .collect(Collectors.toList());
@@ -30,20 +30,24 @@ public class TopicService {
         return Optional.of(topics);
     }
 
+    @Transactional
     public Optional<Topic> getTopic(String id) {
         return topicRepository.findById(id);
     }
 
+    @Transactional
     public Optional<Topic> addTopic(Topic topic) {
         topic = topicRepository.save(topic); //TODO this creates and updates together... wtf...
         return Optional.of(topic);
     }
 
+    @Transactional
     public Optional<Topic> updateTopic(String id, Topic topic) {
         topic = topicRepository.save(topic);
         return Optional.of(topic); //TODO this doesn't distinguish between something new or something updated
     }
 
+    @Transactional
     public Optional<Topic> deleteTopic(String id) {
         Optional<Topic> topic = topicRepository.findById(id);
         topicRepository.deleteById(id); //TODO don't like how this doesn't return anything...
